6b367822c35b1b3be2071e0fcedc9c4b74d5b897
Thomas Hooper <thomash@last.fm>
2018-09-30 12:35:03 +0100

    Remove old curves

--- endless-curve.js
+++ endless-curve.js
@@ -21,31 +21,6 @@ EndlessCurve.prototype.getLtoUmapping = function(l) {
   return l / len;
 };
 
-// EndlessCurve.prototype.getPointAtLength = function(position) {
-//   var p = this.localDistance(position);
-
-//   var len = this.getLengthSafe();
-
-//   if (p < len) {
-//     var u = this.getLtoUmapping(p);
-//     var point = this.getPointAt(u);
-//     return point;
-//   }
-
-//   var newCurve = this.nextCurve();
-//   this.add(newCurve);
-
-//   return this.getPointAtLength(position);
-// };
-
-// EndlessCurve.prototype.getTangentAtLength = function(position) {
-//   var p = this.localDistance(position);
-//   var len = this.getLengthSafe();
-//   var t = p / len;
-//   var tangent = this.getTangentAt(t);
-//   return tangent;
-// };
-
 EndlessCurve.prototype.fillLength = function(length) {
   var p = this.localDistance(length);
 
@@ -74,6 +49,7 @@ EndlessCurve.prototype.removeCurvesBefore = function(position) {
   var lengths = this.getCurveLengths();
   var remove = 0;
   var distanceOffset = 0;
+
   for (var i = 0; i < lengths.length; i++) {
     if (p < lengths[i]) {
       break;
@@ -83,21 +59,17 @@ EndlessCurve.prototype.removeCurvesBefore = function(position) {
   }
   if (remove) {
     this.distanceOffset += distanceOffset;
-    this.slice(remove);
+    this.curves = this.curves.slice(remove);
+    this.guideCurvePath.curves = this.guideCurvePath.curves.slice(remove);
     this.cacheLengths = null;
   }
 };
 
-EndlessCurve.prototype.slice = function(index) {
-  this.curves = this.curves.slice(index);
-};
-
-// Force computeFrenetFrames to use the configured segment
 EndlessCurve.prototype.configureStartEnd = function(position, length) {
   this.fillLength(position + length);
+  this.removeCurvesBefore(position);
 
   position = this.localDistance(position);
-  length = this.localDistance(length);
 
   var len = this.getLengthSafe();
   this.uStart = position / len;
@@ -108,16 +80,5 @@ EndlessCurve.prototype.getBasisAt = function(u) {
   var u2 = this.uStart + this.uLength * u;
   return parent.getBasisAt.call(this, u2);
 };
-
-// EndlessCurve.prototype.getTangentAt = function(u) {
-//   var u2 = this.frenetFramesStart + this.frenetFramesLength * u;
-//   return parent.getTangentAt.call(this, u2);
-// };
-
-// EndlessCurve.prototype.getPointAt = function(u) {
-//   var u2 = this.frenetFramesStart + this.frenetFramesLength * u;
-//   return parent.getPointAt.call(this, u2);
-// };
-
 
 module.exports = EndlessCurve;
